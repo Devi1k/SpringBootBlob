8233123eba1cde7c954016c17e3cb93742e3b435
Devi1k <570541309@qq.com>
2022-10-13 23:59:38 +0800

    2022.10.13 boot&thymeleaf入门

--- src/main/java/com/example/springbootblob/SpringBootBlobApplication.java
+++ src/main/java/com/example/springbootblob/SpringBootBlobApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringBootBlobApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBootBlobApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootBlobApplication.class, args);
+    }
 
 }
--- src/test/java/com/example/springbootblob/SpringBootBlobApplicationTests.java
+++ src/test/java/com/example/springbootblob/SpringBootBlobApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class SpringBootBlobApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
