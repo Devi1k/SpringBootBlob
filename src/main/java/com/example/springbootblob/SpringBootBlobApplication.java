9df30c90abad6374105b7679ba2a2e46a38cc7bb
Devi1k <570541309@qq.com>
2022-10-13 17:39:46 +0800

    Initial commit

---
+++ src/main/java/com/example/springbootblob/SpringBootBlobApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootblob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBlobApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootBlobApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springbootblob/SpringBootBlobApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootblob;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootBlobApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
