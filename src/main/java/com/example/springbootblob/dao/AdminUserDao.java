0cea9857664d7e16f7d6bbf2d8c181506a7200f4
Devi1k <570541309@qq.com>
2022-10-16 00:20:37 +0800

    2022.10.15 分页

--- src/main/java/com/example/springbootblob/dao/AdminUserDao.java
+++ src/main/java/com/example/springbootblob/dao/AdminUserDao.java
@@ -6,12 +6,6 @@ import org.apache.ibatis.annotations.Param;
 import java.util.List;
 import java.util.Map;
 
-/**
- * @author 13
- * @qq交流群 796794009
- * @email 2449207463@qq.com
- * @link http:13blog.site
- */
 public interface AdminUserDao {
 
     /**
