9339b37b46a96cc961fd0f26fb7616742d84256a
Devi1k <570541309@qq.com>
2022-11-13 10:32:20 +0800

    2022.11.12 add interceptor

--- src/main/java/com/example/springbootblog/config/MyBlogWebMvcConfigurer.java
+++ src/main/java/com/example/springbootblog/config/MyBlogWebMvcConfigurer.java
@@ -1,16 +1,17 @@
 package com.example.springbootblog.config;
 
 import com.example.springbootblog.interceptor.AdminLoginInterceptor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import javax.annotation.Resource;
+
 @Configuration
 public class MyBlogWebMvcConfigurer implements WebMvcConfigurer {
-    @Autowired
+    @Resource
     private AdminLoginInterceptor adminLoginInterceptor;
 
     @Value("${upload.file.path}")
@@ -21,8 +22,7 @@ public class MyBlogWebMvcConfigurer implements WebMvcConfigurer {
     }
 
 
-    public void addInterceptor(InterceptorRegistry interceptorRegistry) {
-        interceptorRegistry.addInterceptor(adminLoginInterceptor).addPathPatterns("/admin/**").excludePathPatterns("admin/login")
-                .excludePathPatterns("admin/dist/**").excludePathPatterns("admin/plugins/**");
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(adminLoginInterceptor).addPathPatterns("/admin/**").excludePathPatterns("/admin/login").excludePathPatterns("/admin/dist/**").excludePathPatterns("/admin/plugins/**");
     }
 }
